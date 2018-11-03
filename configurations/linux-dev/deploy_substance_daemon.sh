bb11017892ad4bdb4b4a4dbd56494fc2e29d46de
Davide Pirola <ziocleto@gmail.com>
2018-11-03 15:33:53 +0000

    - Building sat daemon, also deploying it as daemon docker

---
+++ configurations/linux-dev/deploy_substance_daemon.sh
@@ -0,0 +1,13 @@
+sudo mkdir sat
+cd sat
+
+echo -e "FROM "$1"\nADD substance_daemon /\nCMD [\"./substance_daemon\" ]" | tee dockerfile
+
+curl https://49view-api.icu/fs/get/artefacts%2Fsubstance_daemon -o substance_daemon
+sudo chmod 777 substance_daemon 
+
+sudo docker kill $(docker ps --filter "ancestor=sat" -q)
+
+sudo docker build -t sat .
+
+sudo docker run -d sat
