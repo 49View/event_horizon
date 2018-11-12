a7a49e6e2debfb3d86471b6b178ecd53dd877990
Davide Pirola <ziocleto@gmail.com>
2018-11-12 02:17:49 +0000

    - Adding dockerfile for editor

--- configurations/linux-dev/deploy.sh
+++ configurations/linux-dev/deploy.sh
@@ -5,7 +5,10 @@ cp /etc/letsencrypt/live/eventhorizon.pw/fullchain.pem ./event_horizon/node-api-
 cp /etc/letsencrypt/live/eventhorizon.pw/privkey.pem ./event_horizon/node-api-server/bin/server-key.pem
 
 sudo docker kill $(docker ps --filter "ancestor=49view/node-api-server" -q)
+sudo docker build -t eventhorizon-api ./event_horizon/node-api-server
+sudo docker run -p 443:3000 -d eventhorizon-api
 
-sudo docker build -t 49view/node-api-server ./event_horizon/node-api-server
+sudo docker kill $(docker ps --filter "ancestor=49view/node-api-server" -q)
+sudo docker build -t eventhorizon-editor ./event_horizon/devtools/editor
+sudo docker run -p 8000:8000 -d eventhorizon-editor
 
-sudo docker run -p 443:3000 -d 49view/node-api-server
