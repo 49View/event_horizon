9b64c1dd5a77a63567a2376a1bdc08d656e31e3a
Davide Pirola <ziocleto@gmail.com>
2018-11-13 18:58:43 +0000

    - Served editor in his own app

--- node-api-server/routes/index.js
+++ node-api-server/routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-  // res.send('Node api status: <img src="https://circleci.com/gh/49View/node-api-server.svg?style=svg&circle-token=0312dab6c17d25ac4cde35689efc24eb1616d3aa">');
+  res.send('Node api status: <img src="https://circleci.com/gh/49View/node-api-server.svg?style=svg&circle-token=0312dab6c17d25ac4cde35689efc24eb1616d3aa">');
 });
 
 module.exports = router;
