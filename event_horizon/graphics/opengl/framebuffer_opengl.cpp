c8244c695999554af3a83711471fffe34fe349d8
Davide Pirola <ziocleto@gmail.com>
2019-05-10 19:28:11 +0100

    Fixed openGLES drawBuffer(s) NONE

--- event_horizon/graphics/opengl/framebuffer_opengl.cpp
+++ event_horizon/graphics/opengl/framebuffer_opengl.cpp
@@ -66,7 +66,7 @@ void Framebuffer::initDepth( std::shared_ptr<TextureManager> tm ) {
     GLCALL( glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL ) );
     GLCALL( glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE ) );
     GLCALL( glFramebufferTexture2D( GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, mRenderToTexture->getHandle(), 0 ) );
-    GLCALL( glDrawBuffer(GL_NONE) );
+    GLCALL( glDrawBuffers(0, GL_NONE) );
     GLCALL( glReadBuffer(GL_NONE) );
 
     checkFrameBufferStatus();
