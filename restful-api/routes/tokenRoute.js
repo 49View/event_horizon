aaab9be55fc06e565b5ee9b9818d9f3d97208917
Davide Pirola <ziocleto@gmail.com>
2019-06-28 20:12:36 +0100

    Update tokenRoute.js

--- restful-api/routes/tokenRoute.js
+++ restful-api/routes/tokenRoute.js
@@ -121,10 +121,10 @@ const getTokenResponse = async (res, req, project, email, password) => {
         email: dbUser.email,
         guest: dbUser.guest
       };
-      tokenInfo.project =
-        project !== ""
-          ? project
-          : await userController.setDefaultUserProject(dbUser._id);
+      tokenInfo.project = "carillo";
+      // project !== ""
+      //   ? project
+      //   : await userController.setDefaultUserProject(dbUser._id);
     }
   } catch (ex) {
     console.log("gettoken failed", ex);
