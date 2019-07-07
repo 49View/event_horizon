2d377a9b63a5c12cf2a64f959e4ba4640a273e85
Davide Pirola <ziocleto@gmail.com>
2019-07-07 13:48:12 +0100

    Added few features for entries, first entity editor, to be completed

--- restful-api/routes/entitiesRoute.js
+++ restful-api/routes/entitiesRoute.js
@@ -8,6 +8,9 @@ const zlib = require("zlib");
 
 router.get("/content/byId/:id", async (req, res, next) => {
   try {
+    const entityId = req.params.id;
+    const project = req.user.project;
+    console.log("User:", req.user);
     const fileData = await entityController.getEntityContent(entityId, project);
 
     fsController.writeFile(res, fileData);
