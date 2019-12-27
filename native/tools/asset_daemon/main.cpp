fab91343080717c02b8f6ef842e356c0c13fc258
Dado <SaraMacbookPro@gmail.com>
2019-12-27 03:28:09 +0100

    adding support for zip archives

--- native/tools/asset_daemon/main.cpp
+++ native/tools/asset_daemon/main.cpp
@@ -28,6 +28,7 @@
 #include <core/image_util.h>
 #include <core/profiler.h>
 #include <ostream>
+#include <zip.h>
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
@@ -121,6 +122,52 @@ std::optional<MongoFileUpload> saveImageToGridFS(MongoBucket bucket, const char 
         return std::nullopt;
     }
 }
+
+
+static int
+get_data(void **datap, size_t *sizep, const char *archive) {
+    /* example implementation that reads data from file */
+    struct stat st;
+    FILE *fp;
+
+    if ((fp = fopen(archive, "r")) == NULL) {
+        if (errno != ENOENT) {
+            fprintf(stderr, "can't open %s: %s\n", archive, strerror(errno));
+            return -1;
+        }
+
+        *datap = NULL;
+        *sizep = 0;
+
+        return 0;
+    }
+
+    if (fstat(fileno(fp), &st) < 0) {
+        fprintf(stderr, "can't stat %s: %s\n", archive, strerror(errno));
+        fclose(fp);
+        return -1;
+    }
+
+    if ((*datap = malloc((size_t)st.st_size)) == NULL) {
+        fprintf(stderr, "can't allocate buffer\n");
+        fclose(fp);
+        return -1;
+    }
+
+    if (fread(*datap, 1, (size_t)st.st_size, fp) < (size_t)st.st_size) {
+        fprintf(stderr, "can't read %s: %s\n", archive, strerror(errno));
+        free(*datap);
+        fclose(fp);
+        return -1;
+    }
+
+    fclose(fp);
+
+    *sizep = (size_t)st.st_size;
+    return 0;
+}
+
+
 
 void elaborateMatSBSAR(MongoBucket entity_bucket,
                        const std::string &mainFileName,
@@ -226,6 +273,49 @@ void parseElaborateStream(mongocxx::change_stream &stream, MongoBucket sourceAss
                                   meta.useremail);
             }
         } else if (meta.group == ResourceGroup::Geom) {
+            if (getFileNameExt(std::string(meta.filename)) == ".zip") {
+                void *data;
+                size_t size;
+                zip_source_t *src;
+                zip_t *za;
+                zip_error_t error;
+                /* get buffer with zip archive inside */
+                if (get_data(&data, &size, fileDownloaded.c_str()) < 0) {
+                    LOGRS("Bollox");
+                }
+                zip_error_init(&error);
+                /* create source from buffer */
+                if ((src = zip_source_buffer_create(data, size, 1, &error)) == NULL) {
+                    fprintf(stderr, "can't create source: %s\n", zip_error_strerror(&error));
+                    free(data);
+                    zip_error_fini(&error);
+                    LOGRS("Bollox2");
+                }
+
+                /* open zip archive from source */
+                if ((za = zip_open_from_source(src, 0, &error)) == NULL) {
+                    fprintf(stderr, "can't open zip from source: %s\n", zip_error_strerror(&error));
+                    zip_source_free(src);
+                    zip_error_fini(&error);
+                    LOGRS("Bollox3");
+                }
+
+                auto numFiles = zip_get_num_entries( za, ZIP_FL_UNCHANGED );
+                LOGRS( "NumFiles: " <<  numFiles );
+                for ( auto t = 0; t < numFiles; t++ ) {
+                    auto zFile = zip_fopen_index( za, t, ZIP_FL_UNCHANGED );
+                    zip_stat_t zInfo;
+                    zip_stat_index( za, t, ZIP_FL_UNCHANGED, &zInfo );
+                    auto buff = make_uint8_p( zInfo.size );
+                    auto byteRead = zip_fread( zFile, buff.first.get(), buff.second );
+                    LOGRS(  zip_get_name( za, t, ZIP_FL_UNCHANGED) << " Size: " << buff.second << " Uncompressed: " << zInfo.size << " Read: " << byteRead );
+                    auto tempFileName = getDaemonRoot() + "/" + zInfo.name;
+                    FM::writeLocalFile( tempFileName, SerializableContainer{buff.first.get(), buff.first.get()+buff.second} );
+                }
+
+//                auto container = FM::readLocalFileC(fileDownloaded);
+//                LOGRS(std::string(meta.filename));
+            }
             if (getFileNameExt(std::string(meta.filename)) == ".fbx") {
                 elaborateGeomFBX(entityBucket, fileDownloaded, meta.project, meta.username, meta.useremail);
             }
