47f735ede7a11593cf92af82aa240717d42bd273
DadoLinux <ziocleto@gmail.com>
2020-02-23 12:10:59 +0000

    updated makefile and build machine to use GTK in order to quickly run opencv-playground tests

--- build_machine.sh
+++ build_machine.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y libssl-dev apt-transport-https libglew-dev libglfw3 libglfw3-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev libopenal-dev libstdc++-9-dev libbz2-dev libjpeg-dev libpng-dev libtiff-dev libwebp-dev nettle-dev gnutls-dev libicu-dev npm 
+sudo apt-get install -y libssl-dev apt-transport-https libglew-dev libglfw3 libglfw3-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev libopenal-dev libstdc++-9-dev libbz2-dev libjpeg-dev libpng-dev libtiff-dev libwebp-dev nettle-dev gnutls-dev libicu-dev libgtk2.0-dev npm
 
 pom-get.sh openssl 			    /usr/local/
 pom-get.sh mongo-c-driver   /usr/local/
@@ -11,15 +11,16 @@ pom-get.sh rapidjson        /usr/local/
 pom-get.sh openjpeg 		    /usr/local/
 pom-get.sh leptonica        /usr/local/
 pom-get.sh tesseract        /usr/local/
+pom-get.sh opencv           /usr/local/
 
 pom-get.sh stb              /usr/local/
 pom-get.sh websocketpp      /usr/local/
 pom-get.sh boost            /usr/local/
+pom-get.sh tinygltf         /usr/local/include/tinygltf/
+pom-get.sh glm 				      /usr/local/
+
 pom-get.sh opt-sat-indie    /opt/Allegorithmic/
 pom-get.sh fbx2gltf-linux   /usr/local/bin/
-pom-get.sh tinygltf         /usr/local/include/tinygltf/
-pom-get.sh opencv           /usr/local/
-pom-get.sh glm 				      /usr/local/
 
 sudo gitbuild-make.sh openssl OpenSSL_1_1_1d openssl "-DBUILD_TOOLS=FALSE -DBUILD_EXAMPLES=FALSE" /usr/local
 sudo gitbuild.sh mongo-c-driver 1.15.3 mongodb "-DBUILD_TOOLS=FALSE -DBUILD_EXAMPLES=FALSE" /usr/local
@@ -32,6 +33,8 @@ sudo gitbuild.sh rapidjson v1.1.1 ziocleto "" /usr/local
 sudo gitbuild.sh openjpeg v2.3.1 uclouvain "" /usr/local
 sudo gitbuild.sh leptonica 1.79.0 DanBloomberg "" /usr/local
 sudo gitbuild.sh tesseract 4.1.1 tesseract-ocr "-DSTATIC=TRUE" /usr/local
+
+sudo gitbuild.sh opencv 4.2.0 opencv "-DWITH_OPENEXR=FALSE -DBUILD_JAVA=FALSE -DBUILD_TESTS=FALSE -DBUILD_EXAMPLES=FALSE" /usr/local
 
 # Install dev environment
 
