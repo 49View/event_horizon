05b23879e998dddbe8c73cddf31b3f387160ac3c
DadoLinux <ziocleto@gmail.com>
2020-02-24 14:42:16 +0000

    Added graphql api routing

--- bootstrap_local.sh
+++ bootstrap_local.sh
@@ -1,5 +1,3 @@
-gnome-terminal --window-with-profile=eh_bootstrap -- bash -c 'sudo systemctl restart nginx && tail -f /var/log/nginx/access.log'
+gnome-terminal --window-with-profile=eh_bootstrap -- bash -c 'systemctl restart nginx && tail -f /var/log/nginx/access.log'
 
-gnome-terminal --window-with-profile=eh_bootstrap -- bash -c 'export EH_MONGO_DEFAULT_DB=$1 && sudo mongod --port 27017 --replSet rs0'
-
-gnome-terminal --window-with-profile=eh_bootstrap -- bash -c 'cd ./restful-api && nodemon --max-old-space-size=16000 ./bin/www'
+gnome-terminal --window-with-profile=eh_bootstrap -- /bin/bash -c 'mongod --port 27017 --replSet rs0'
