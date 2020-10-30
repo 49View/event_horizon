ed226946f18a2c1eb01c346bb565c0e9e63ab87b
DadoLinux <ziocleto@gmail.com>
2020-10-30 10:54:18 +0000

    Starting to get serious on OsmOrchestrator

--- native/poly/osm/osm_orchestrator.cpp
+++ native/poly/osm/osm_orchestrator.cpp
@@ -3,9 +3,12 @@
 //
 
 #include "osm_orchestrator.hpp"
+
+#include <core/http/webclient.h>
+#include <core/descriptors/osm_bsdata.hpp>
 #include <poly/scene_graph.h>
 #include <poly/collision_mesh.hpp>
-#include "osm_names.hpp"
+#include <poly/osm/osm_names.hpp>
 
 namespace HOD { // HighOrderDependency
 
@@ -24,40 +27,22 @@ namespace HOD { // HighOrderDependency
 
 std::shared_ptr<CollisionMesh> OSMService::createCollisionMesh( const OSMData *osm ) {
     auto ret = std::make_shared<CollisionMesh>();
-
-//    ret->bbox = house->BBox();
-//    std::set<int64_t> doorAddedSet{};
-//    for ( const auto& f : house->mFloors ) {
-//        CollisionGroup cg{};
-//        cg.bbox = f->BBox();
-//        for ( const auto& r : f->rooms ) {
-//            CollisionRoom cr{};
-//            cr.bbox = r->BBox();
-//            for ( const auto& ls : r->mWallSegmentsSorted ) {
-//                if ( !checkBitWiseFlag(ls.tag, WF_IsDoorPart) ) {
-//                    cr.collisionCollisionElement.emplace_back(ls.p1, ls.p2, ls.normal);
-//                }
-//            }
-//
-//            for ( const auto& doorHash : r->doors ) {
-//                for ( const auto& door : f->doors ) {
-//                    if ( door->hash == doorHash && doorAddedSet.find(doorHash) == doorAddedSet.end() ) {
-//                        cr.collisionCollisionElement.emplace_back(door->us1.points[1], door->us1.points[2],
-//                                                                  door->us1.inwardNormals[0]);
-//                        cr.collisionCollisionElement.emplace_back(door->us2.points[1], door->us2.points[2],
-//                                                                  door->us2.inwardNormals[0]);
-//                        if ( door->isMainDoor ) {
-//                            cr.collisionCollisionElement.emplace_back(door->us2.middle, door->us1.middle,
-//                                                                      door->us1.crossNormals[0]);
-//                        }
-//                        cr.bbox.merge(door->BBox());
-//                        doorAddedSet.insert(doorHash);
-//                    }
-//                }
-//            }
-//            cg.collisionRooms.emplace_back(cr);
-//        }
-//        ret->collisionGroups.emplace_back(cg);
-//    }
     return ret;
+}
+
+void OSMService::loadOSM( float lon, float lat, int zoomLevel ) {
+
+    Http::getNoCache(Url{ "/osm/" + std::to_string(lon) + "/" + std::to_string(lat) + "/" + std::to_string(zoomLevel) },
+                     [&]( HttpResponeParams params ) {
+                         if ( !params.BufferString().empty() ) {
+                             auto newHouseInstance = std::make_shared<OSMData>(params.BufferString());
+//                             HOD::resolver<OSMData>(sg, nullptr, [&]() {
+//                                 OSMData map{FM::readLocalFileC("../../elements.json")};
+//                                 sg.loadCollisionMesh(OSMService::createCollisionMesh(&map));
+//                                 sg.setCollisionEnabled(true);
+//                                 sg.GB<GT::OSMTile>(&map, V2f{-0.1344f, 51.4892f}, GT::Tag(SHADOW_MAGIC_TAG), GT::Bucket(GTBucket::NearUnsorted), GT::M("city,atlas"), GT::Program(S::SH_CITY_ATLAS));
+//                                 sg.GB<GT::OSMBuildings>(&map, V2f{-0.1344f, 51.4892f}, GT::Bucket(GTBucket::Near), GT::M("city,atlas"), GT::Program(S::SH_CITY_ATLAS));
+//                             });
+                         }
+                     });
 }
--- native/poly/osm/osm_orchestrator.hpp
+++ native/poly/osm/osm_orchestrator.hpp
@@ -11,4 +11,9 @@ struct OSMData;
 
 namespace OSMService {
     std::shared_ptr<CollisionMesh> createCollisionMesh( const OSMData *house );
+    void loadOSM( float lon, float lat, int zoomLevel );
 }
+
+class OsmOrchestrator {
+
+};
